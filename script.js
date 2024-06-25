fad7628b027d901441db3209b4a0c243b7a57156
maureen <bisiattah@yahoo.com>
2024-06-25 20:38:31 +0100

    updated

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ const dice = document.getElementById('dice');
 const baseUrl = 'https://api.adviceslip.com/advice'
  
 const getAdvice = async () => {
+    try {
     const response = await fetch(baseUrl);
     const data = await response.json();
     console.log(data.slip);
     advice.innerHTML = data.slip.advice;
-    number.innerHTML = data.slip.id;
+    number.innerHTML = `#${data.slip.id}`;
+        
+    } catch (error) {
+    console.error('Error occur', error);
+    advice.innerHTML = "Error fetching advice";
+    number.innerHTML = '"please try again later"';
+    }
 }
 
 // getAdvice();
